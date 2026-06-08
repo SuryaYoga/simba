d68c5bb15ccaeb999836820d97d865c19a480bd3
Surya Yoga <suryaadit832@gmail.com>
2026-06-08 10:50:51 +0800

    fix reset password pages v3

--- app/(auth)/confirm/route.js
+++ app/(auth)/confirm/route.js
@@ -1,5 +1,6 @@
-import { createClient } from '@/lib/supabase'
+import { createServerClient } from '@supabase/ssr'
 import { NextResponse } from 'next/server'
+import { cookies } from 'next/headers'
 
 export async function GET(request) {
   const { searchParams, origin } = new URL(request.url)
@@ -7,8 +8,25 @@ export async function GET(request) {
   const type = searchParams.get('type')
 
   if (token_hash && type) {
-    const supabase = createClient()
+    const cookieStore = await cookies()
+
+    const supabase = createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+      {
+        cookies: {
+          getAll: () => cookieStore.getAll(),
+          setAll: (cookiesToSet) => {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options)
+            )
+          },
+        },
+      }
+    )
+
     const { error } = await supabase.auth.verifyOtp({ type, token_hash })
+
     if (!error) {
       return NextResponse.redirect(`${origin}/auth/reset-password`)
     }
